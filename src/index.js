003c081510463bc849b3bc9d6730edf25eaa49d3
Nika Kakhiani <nikakakhi@gmail.com>
2023-08-31 02:07:37 +0400

    effects and data fetching

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
+import "./index.css";
+import App from "./App";
 
-import StarRating from "./StarRating";
+// import StarRating from "./StarRating";
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
+// function Test() {
+// const [movieRating, setMovieRating] = useState(0);
 
-  return (
-    <div>
-      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />;
-      <p>This movies was rated {movieRating} stars</p>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />;
+//       <p>This movies was rated {movieRating} stars</p>
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
-      maxRating={5}
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-    />
-    <StarRating size={24} color="red" className="test" defaultRating={3} />
-    <Test />
+    <App />
+    {/* <StarRating */}
+    {/* maxRating={5}
+    messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]} */}
+    {/* /> */}
+    {/* <StarRating size={24} color="red" className="test" defaultRating={3} /> */}
+    {/* <Test /> */}
   </React.StrictMode>
 );
